be67d002a0143eea42f64bd2c201a6cf8975aff8
Muhammed Enes Yılmaz <menes.yilm@gmail.com>
2026-04-29 11:28:05 +0300

    feat: add Stock and PriceSnapshot domain models.

---
+++ Rasyonet_HW.API/Models/PriceSnapshot.cs
@@ -0,0 +1,19 @@
+﻿namespace Rasyonet_HW.API.Models
+{
+    public class PriceSnapshot
+    {
+        public int Id { get; set; }
+        public int StockId { get; set; }
+
+        public decimal CurrentPrice { get; set; }
+        public decimal OpenPrice { get; set; }
+        public decimal HighPrice { get; set; }
+        public decimal LowPrice { get; set; }
+        public decimal PreviousClose { get; set; }
+        public decimal ChangePercent { get; set; }
+
+        public DateTime FetchedAt { get; set; } = DateTime.UtcNow;
+        public Stock Stock { get; set; } = null!;
+
+    }
+}
